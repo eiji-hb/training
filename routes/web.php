2bdae73f842cccca079ca83aa5e833ed6cc2b7cf
eiji-hb <eijisugiurar@gmail.com>
2020-07-03 10:32:14 +0900

    コントローラー、view作成

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/','TaskController@index')->name('root');
+Route::get('create', 'TaskController@create')->name('task.create');
