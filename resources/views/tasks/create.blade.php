213254bab64ceedc23117d1d6956e185c84a638d
eiji-hb <eijisugiurar@gmail.com>
2020-07-03 15:50:25 +0900

    view完成

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,1 +1,22 @@
-create
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-10">
+        <form method="POST" action="{{route('task.store')}}">
+          @csrf
+          <div class="form-group">
+            <label for="titleInput">タイトル</label>
+            <input type="text" class="form-control" name="name">
+          </div>
+          <div class="form-group">
+            <label for="bodyInput">内容</label>
+            <textarea class="form-control" id="bodyInput" rows="3" name="description"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">新規追加</button>
+        </form>
+      </div>
+    </div>
+  </div>
+@endsection
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,1 +1,33 @@
-index
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        <a href="{{ route('task.create') }}" class="btn btn-primary mr-3">新規作成</a>
+        <table class="table">
+          <thead>
+            <tr>
+            <th scope="col">id</th>
+            <th scope="col">name</th>
+            <th scope="col">description</th>
+            <th scope="col">登録日時</th>
+            <th scope="col">詳細</th>
+            </tr>
+          </thead>
+          <tbody>
+              @foreach($tasks as $task)
+            <tr>
+              <td>{{ $task->id}}</td>
+              <td>{{ $task->name}}</td>
+              <td>{{ $task->description}}</td>
+              <td>{{ $task->created_at}}</td>
+              <td><a href="{{ route('task.show', ['id' => $task->id ]) }}" class="btn btn-primary">詳細</a></td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+@endsection
